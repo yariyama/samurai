eb5139dd06f1183da4c714ae89505f7710ce1cff
yariyama <86090612+yariyama@users.noreply.github.com>
2021-07-22 23:12:18 +0900

    Add files via upload

---
+++ Car3Manager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car3Manager : MonoBehaviour
+{
+    //座標
+    private Vector2 _position;
+    //スケール
+    private Vector2 _scale;
+
+    //ステータス
+    private int _st;
+    //横方向
+    private int _dire_x;
+    //縦方向
+    private int _dire_y;
+    //スピード
+    public float _speed;
+
+    //_st=1-基本形
+    //_st=2-移動
+
+    //_dire_x=1-右
+    //_dire_x=2-左
+    //_dire_y=1-上
+    //_dire_y=2-下
+
+    void Awake()
+    {
+        _position = transform.position;
+        _scale = transform.localScale;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _st = 1;
+    }
+
+    void FixedUpdate()
+    {
+        if (_st==2)
+        {
+            if (_dire_x==1)
+            {
+                _position.x += _speed/50;
+                if (_position.x>=6)
+                {
+                    _position.x = 6;
+                }
+            }
+            else if (_dire_x == 2)
+            {
+                _position.x -= _speed/50;
+                if (_position.x <= -6)
+                {
+                    _position.x = -6;
+                }
+            }
+            if (_dire_y == 1)
+            {
+                _position.y += _speed/50;
+                if (_position.y >= 4.5f)
+                {
+                    _position.y = 4.5f;
+                }
+            }
+            else if (_dire_y == 2)
+            {
+                _position.y -= _speed/50;
+                if (_position.y <= -4.5f)
+                {
+                    _position.y = -4.5f;
+                }
+            }
+            transform.position = _position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _st = 1;
+        _dire_x = 0;
+        _dire_y = 0;
+        if (Input.GetKey("right"))
+        {
+            _st = 2;
+            _dire_x = 1;
+            _scale.x = 1;
+        }
+        else if (Input.GetKey("left"))
+        {
+            _st = 2;
+            _dire_x = 2;
+            _scale.x = -1;
+        }
+        transform.localScale = _scale;
+
+        if (Input.GetKey("up"))
+        {
+            _st = 2;
+            _dire_y = 1;
+        }
+        else if (Input.GetKey("down"))
+        {
+            _st = 2;
+            _dire_y = 2;
+        }
+    }
+}
