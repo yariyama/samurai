c19d4ac1703094fa1802c14bf7b8a6d9225897e4
yariyama <86090612+yariyama@users.noreply.github.com>
2021-07-24 09:31:27 +0900

    Add files via upload

---
+++ Uni1Manager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Uni1Manager : MonoBehaviour
+{
+    //カウントゲームオブジェクト
+    private GameObject _Count;
+
+    //カウントテキスト
+    private Text _count_text;
+
+    //クリックカウント
+    private int _click_c;
+
+    void Awake()
+    {
+        _Count = GameObject.Find("Count");
+        _count_text = _Count.GetComponent<Text>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _click_c = 0;
+    }
+
+    void OnMouseDown()
+    {
+        ++_click_c;
+        _count_text.text = _click_c.ToString("00");
+    }
+}
