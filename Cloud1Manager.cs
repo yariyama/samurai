05c6c957cb6d591e6ab52ea05306634f807b6776
yariyama <86090612+yariyama@users.noreply.github.com>
2021-07-27 19:19:33 +0900

    Add files via upload

---
+++ Cloud1Manager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud1Manager : MonoBehaviour
+{
+    //ステータス
+    private int _st;
+    //スピード
+    public float _speed;
+
+    //_st=1-基本形
+    //_st=2-移動
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _st = 1;
+    }
+
+    void FixedUpdate()
+    {
+        if (_st==2)
+        {
+            transform.Translate(0,_speed/50,0);
+        }
+    }
+
+    public void MoveSet(int _no)
+    {
+        _st = _no;
+    }
+}
---
+++ UpManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpManager : MonoBehaviour
+{
+    private GameObject _Cloud1;
+    private Cloud1Manager _Cloud1Manager;
+
+    public int _ver;
+
+    void Awake()
+    {
+        _Cloud1 = GameObject.Find("Cloud1");
+        _Cloud1Manager = _Cloud1.GetComponent<Cloud1Manager>();
+    }
+
+    void OnMouseDown()
+    {
+        if (_ver==1) {
+            _Cloud1Manager.MoveSet(2);
+        }
+        else
+        {
+            _Cloud1Manager.MoveSet(1);
+        }
+    }
+}
