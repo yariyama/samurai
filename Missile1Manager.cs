e5d282404083b10eab191170a7e8c3f8c5035685
yariyama <86090612+yariyama@users.noreply.github.com>
2021-07-30 23:14:27 +0900

    Add files via upload

---
+++ Missile1Manager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile1Manager : MonoBehaviour
+{
+    //ステータス
+    private int _st;
+    //スピード
+    public float _speed;
+
+    //_st=1-基本形
+    //_st=2-移動
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _st = 1;
+    }
+
+    void FixedUpdate()
+    {
+        if(_st==2)
+        {
+            transform.Translate(0,_speed/50,0);
+        }
+    }
+
+    public void MoveSet()
+    {
+        if (_st==1) {
+            _st = 2;
+        }
+    }
+}
---
+++ Rocket1Manager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket1Manager : MonoBehaviour
+{
+    //ミサイル1オブジェクト
+    private GameObject _Missile1;
+    //ミサイル1スクリプト
+    private Missile1Manager _Missile1Manager;
+
+    //回転スピード
+    public float _speed;
+
+
+    void Awake()
+    {
+        _Missile1 = transform.Find("Missile1").gameObject;
+        _Missile1Manager = _Missile1.GetComponent<Missile1Manager>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.Rotate(0,0,_speed/50);
+    }
+
+    void OnMouseDown()
+    {
+        _Missile1Manager.MoveSet();
+    }
+}
