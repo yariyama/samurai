9aa593ed3467af6c7d0ff46026209306d26f1a1e
yariyama <86090612+yariyama@users.noreply.github.com>
2021-07-31 16:58:30 +0900

    Add files via upload

---
+++ RotationManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationManager : MonoBehaviour
+{
+    //ステータス
+    private int _st;
+    //回転量
+    public float _angle;
+
+    //_st=1-基本形
+    //_st=2-回転
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _st = 1;
+        _angle = 90;
+    }
+
+    void FixedUpdate()
+    {
+        if (_st==2) {
+            transform.Rotate(0, 0, _angle / 50);
+        }
+    }
+
+    public void RoteSet()
+    {
+        if (_st==1)
+        {
+            _st = 2;
+        }
+        else
+        {
+            _st = 1;
+        }
+    }
+}
---
+++ TouchManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour
+{
+    //クリックオブジェクト
+    private GameObject _ClickedGameObject;
+    //回転スクリプト
+    private RotationManager _RotationManager;
+
+    //タッチ処理用
+    private Vector2 touchPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //マウスの画面位置の取得
+            touchPosition = Input.mousePosition;
+            TouchAct(touchPosition);
+        }
+    }
+
+    /// タッチされていたときの処理
+    private void TouchAct(Vector2 pos)
+    {
+        //マウスの画面位置をゲーム中の位置に変換
+        Vector2 worldPoint = Camera.main.ScreenToWorldPoint(pos);
+
+        //クリックしたオブジェクトを全て取得する
+        foreach (RaycastHit2D hit in Physics2D.RaycastAll(worldPoint, Vector2.zero))
+        {
+            //オブジェクトが見つかったときの処理
+            if (hit)
+            {
+                _ClickedGameObject = hit.transform.gameObject;
+                if (_ClickedGameObject.tag == "Player")
+                {
+                    //サーチしたオブジェクトのスクリプトを取得
+                    _RotationManager = _ClickedGameObject.GetComponent<RotationManager>();
+                    _RotationManager.RoteSet();
+                }
+            }
+        }
+    }
+}
