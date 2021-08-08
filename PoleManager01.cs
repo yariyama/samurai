2b8ce538eec7797c3c39b186dcf0f374c8e73439
yariyama <86090612+yariyama@users.noreply.github.com>
2021-08-08 13:27:41 +0900

    Add files via upload

---
+++ PoleManager01.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoleManager01 : MonoBehaviour
+{
+    //表示
+    private MeshRenderer _renderer;
+
+    //ステータス
+    private int _st;
+
+    void Awake()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer.enabled = false;
+        _st = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
