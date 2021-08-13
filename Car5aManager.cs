c8b98f5994a58aa5f32b748bfd15c9674cf4e279
yariyama <86090612+yariyama@users.noreply.github.com>
2021-08-13 10:33:55 +0900

    Add files via upload

---
+++ Car5aManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car5aManager : MonoBehaviour
+{
+    //ステータス
+    private int _st;
+    //スピード
+    public float _speed;
+
+    //_st=1-基本形
+    //_st=2-移動
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _st = 1;
+    }
+
+    void FixedUpdate()
+    {
+        if (_st==2)
+        {
+            transform.Translate(_speed/50,0,0);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        _st = 2;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+            _st = 1;
+    }
+}
---
+++ Car5bManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car5bManager : MonoBehaviour
+{
+    //リッジドボディ
+    private Rigidbody2D _rbody;
+
+    //ステータス
+    private int _st;
+    //スピード
+    public float _speed;
+
+    //_st=1-基本形
+    //_st=2-移動
+
+    void Awake()
+    {
+        _rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _st = 1;
+    }
+
+    void FixedUpdate()
+    {
+        if (_st == 2)
+        {
+            //リッジドボディを利用して動かす
+            _rbody.velocity = new Vector2(_speed,0);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        _st = 2;
+    }
+}
