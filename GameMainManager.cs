fec272183b8acc190fab6c12ba904ce92fff2c5a
yariyama <86090612+yariyama@users.noreply.github.com>
2021-10-01 00:45:16 +0900

    Add files via upload

---
+++ GameMainManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMainManager : MonoBehaviour
+{
+    //音符プレハブ
+    public GameObject _Note_pf;
+    //音符オブジェクト
+    private GameObject _Note;
+    //音符スクリプト
+    private NoteManager _NoteManager;
+
+    //ステータス
+    private int _st;
+    //タイマー
+    private float _timer;
+
+    //_st=1-基本
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _st = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (_st==1)
+        {
+            _timer += Time.deltaTime;
+            if (_timer>=0.5f)
+            {
+                _timer = 0;
+                NoteSet();
+            }
+        }
+    }
+
+    //音符セット
+    public void NoteSet()
+    {
+        _Note = Instantiate(_Note_pf) as GameObject;
+    }
+}
---
+++ NoteManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    //ステータス
+    private int _st;
+
+    //ノーマルスピード
+    public float _n_speed;
+
+    //_st=1-基本移動
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _st = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        //基本移動
+        if (_st==1)
+        {
+            transform.Translate(-_n_speed/50,0,0);
+        }
+    }
+}
