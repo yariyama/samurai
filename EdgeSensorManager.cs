8ea0e71bd2c8adf446277ab58df1aa2683ea8cf8
yariyama <86090612+yariyama@users.noreply.github.com>
2021-10-16 21:03:37 +0900

    Add files via upload

---
+++ EdgeSensorManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeSensorManager : MonoBehaviour
+{
+    private GameObject _Monkey;
+    private MonkeyManager _MonkeyManager;
+
+    void Awake()
+    {
+        _Monkey = transform.parent.gameObject;
+        _MonkeyManager = _Monkey.GetComponent<MonkeyManager>();
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        _MonkeyManager.TurnSet();
+    }
+}
