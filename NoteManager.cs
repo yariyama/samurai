f9426bef6347f04aaf9159fcf7ea5ace47219313
yariyama <86090612+yariyama@users.noreply.github.com>
2021-10-29 01:40:10 +0900

    Add files via upload

---
+++ NoteManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    //ゲームメインオブジェクト
+    private GameObject _GameMain;
+    //ゲームメインスクリプト
+    private GameMainManager _GameMainManager;
+
+    //座標
+    private Vector2 _position;
+    //アニメーター
+    private Animator _animator;
+
+    //ステータス
+    private int _st;
+    //バージョン
+    public int _ver;
+    //ラインNO
+    private int _line_no;
+    //色タイプ
+    private int _color_tp;
+
+    //ノーマルスピード
+    public float _n_speed;
+    //ダメスピード
+    public float _d_speed;
+
+    //_st=1-基本移動
+    //_st=2-ダメージ
+
+    void Awake()
+    {
+        _GameMain = GameObject.Find("GameMain");
+        _GameMainManager = _GameMain.GetComponent<GameMainManager>();
+
+        _position = transform.position;
+        _animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        //基本移動
+        if (_st==1)
+        {
+            transform.Translate(-_n_speed/50,0,0);
+
+            if (transform.position.x<=-4)
+            {
+                _st = 2;
+                if (_color_tp == 1)
+                {
+                    _animator.Play("red_dame");
+                }
+                else if (_color_tp == 2)
+                {
+                    _animator.Play("blue_dame");
+                }
+                else if (_color_tp == 3)
+                {
+                    _animator.Play("yellow_dame");
+                }
+            }
+        }
+        //ダメージ
+        else if (_st==2)
+        {
+            transform.Translate(-_d_speed / 50, 0, 0);
+
+            if (transform.position.x <= -7.7f)
+            {
+                _GameMainManager._note_st[_ver] = false;
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    //アクティブ化
+    public void ActiveSet(int _no)
+    {
+        _line_no = _no;
+        _st = 1;
+
+        _color_tp = Random.Range(1,4);
+        if (_color_tp==1)
+        {
+            _animator.Play("red_base");
+        }
+        else if (_color_tp == 2)
+        {
+            _animator.Play("blue_base");
+        }
+        else if (_color_tp == 3)
+        {
+            _animator.Play("yellow_base");
+        }
+
+        _position.x = 7.6f;
+        _position.y = 3.5f-2*(_line_no-1);
+        transform.position = _position;
+    }
+}
---
+++ SafeAreaManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeAreaManager : MonoBehaviour
+{
+    //表示
+    private SpriteRenderer _renderer;
+    //色
+    private Color _color;
+
+    //ステータス
+    private int _st;
+    //タイマー
+    private float _timer;
+    //プッシュステータス
+    private bool _push_st;
+
+    //_st=1-基本形
+    //_st=2-アクション
+    //_st=3-失敗
+
+    void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+        _color = _renderer.color;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _st = 1;
+        _timer = 0;
+        _push_st = false;
+        _color.a = 0.4f;
+        _renderer.color = _color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            if (_st==1 && !_push_st)
+            {
+                _push_st = true;
+                _st = 2;
+                _timer = 0;
+                _color.a = 0.7f;
+                _renderer.color = _color;
+            }
+        }
+        else
+        {
+            _push_st = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //アクション
+        if (_st==2)
+        {
+            _timer += Time.deltaTime;
+            if (_timer>=0.1f)
+            {
+                _timer = 0;
+                _st = 1;
+                _color.a = 0.4f;
+                _renderer.color = _color;
+            }
+        }
+    }
+}
