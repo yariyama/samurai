7b4de49487101eae024cedd3d7224291a6b7c9c6
yariyama <86090612+yariyama@users.noreply.github.com>
2021-10-28 12:38:06 +0900

    Add files via upload

---
+++ AttackHItManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackHItManager : MonoBehaviour
+{
+    //パンダオブジェクト
+    private GameObject _Panda;
+    //パンダスクリプト
+    private PandaManager _PandaManager;
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name=="Panda")
+        {
+            _Panda = collision.gameObject;
+            _PandaManager = _Panda.GetComponent<PandaManager>();
+            if (_PandaManager._st!=9 && _PandaManager._st != 10 && !_PandaManager._jump_down)
+            {
+                _PandaManager.DameSet();
+            }
+        }
+    }
+}
