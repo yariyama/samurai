b5c9041ba6ddea0a3d99e3e1557e48fdf24835f3
yariyama <86090612+yariyama@users.noreply.github.com>
2021-11-11 17:49:25 +0900

    Add files via upload

---
+++ DelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelManager : MonoBehaviour
+{
+    //オーディオソース★
+    private AudioSource _audio;
+    //効果音★
+    public AudioClip _se1;
+
+    void Awake()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
+    public void ButtonPush()
+    {
+        PlayerPrefs.DeleteAll();
+
+        //★
+        _audio.clip = _se1;
+        _audio.Play();
+    }
+}
