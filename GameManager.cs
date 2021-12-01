0d53d2e7379f4f13d03c85f49721028727300a9e
yariyama <86090612+yariyama@users.noreply.github.com>
2021-12-01 14:48:53 +0900

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //オーディオソース
+    private AudioSource _audio;
+    //BGM
+    public AudioClip _bgm1;
+
+    //ライフ
+    public static float _life;
+    //スコア★
+    public static int _score;
+
+    void Awake()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _life = 3;
+        //★
+        _score = 0;
+        _audio.clip = _bgm1;
+        _audio.Play();
+    }
+
+    public void BGMStop()
+    {
+        _audio.Stop();
+    }
+}
---
+++ ItemManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//★
+using UnityEngine.UI;
+
+
+public class ItemManager : MonoBehaviour
+{
+    //パンダオブジェクト
+    private GameObject _Panda;
+    //パンダスクリプト
+    private PandaManager _PandaManager;
+    //スコアオブジェクト★
+    private GameObject _Score;
+
+    //アニメーター
+    private Animator _animator;
+    //スコアテキスト★
+    private Text _score_text;
+    //オーディオソース
+    private AudioSource _audio;
+    //効果音
+    public AudioClip _se1;
+    public AudioClip _se2;
+
+    //ステータス
+    private int _st;
+    //タイプ
+    public int _tp;
+
+    //_st=1-基本形
+    //_st=2-ゲット
+
+    void Awake()
+    {
+        _Panda = GameObject.Find("Panda");
+        _PandaManager = _Panda.GetComponent<PandaManager>();
+        _animator = GetComponent<Animator>();
+        _audio = GetComponent<AudioSource>();
+        //★
+        _Score = GameObject.Find("Score");
+        _score_text = _Score.GetComponent<Text>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _st = 1;
+        _animator.Play("base");
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name=="Panda"&&_st==1)
+        {
+            if (_tp==2) {
+                _PandaManager.SDameSet();
+                _audio.clip = _se2;
+            }
+            else
+            {
+                //★
+                GameManager._score += 10;
+                _score_text.text = GameManager._score.ToString("0000");
+
+                _audio.clip = _se1;
+            }
+            _st = 2;
+            _animator.Play("get");
+            _audio.Play();
+        }
+    }
+
+    void DelSet()
+    {
+        Destroy(this.gameObject);
+    }
+}
