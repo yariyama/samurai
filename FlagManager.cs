2a935182b4dfb28721513e3c42905ae0b3da6d6c
yariyama <86090612+yariyama@users.noreply.github.com>
2021-12-09 12:37:03 +0900

    Add files via upload

---
+++ FlagManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagManager : MonoBehaviour
+{
+    private GameObject _Panda;
+    private PandaManager _PandaManager;
+
+    private bool _hit_st;
+
+
+    void Awake()
+    {
+        _Panda = GameObject.Find("Panda");
+        _PandaManager = _Panda.GetComponent<PandaManager>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _hit_st=true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name=="Panda" &&(_PandaManager._st==1|| _PandaManager._st == 2|| _PandaManager._st == 3)&&_hit_st)
+        {
+            _PandaManager.WinSet();
+            _hit_st = false;
+        }
+    }
+}
---
+++ StageClearManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageClearManager : MonoBehaviour
+{
+    //ゲームメインオブジェクト
+    private GameObject _GameMain;
+    //ゲームマネージャー
+    private GameManager _GameManager;
+
+    //座標
+    private Vector3 _position;
+    //オーディオソース
+    private AudioSource _audio;
+    //効果音
+    public AudioClip _se1;
+
+    //ステータス
+    private int _st;
+
+    //_st=1-基本形
+    //_st=2-スライド
+
+
+    void Awake()
+    {
+        _GameMain = GameObject.Find("GameMain");
+        _GameManager = _GameMain.GetComponent<GameManager>();
+
+        _position = transform.localPosition;
+        _audio = GetComponent<AudioSource>();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (_st==1)
+        {
+
+        }
+        else if (_st==2)
+        {
+            _position.x -= 30f;
+            if (_position.x<=0)
+            {
+                _position.x = 0;
+                _st = 1;
+            }
+            transform.localPosition = _position;
+        }
+    }
+
+    public void ActiveSet()
+    {
+        _st = 2;
+        _position.x = 770;
+        transform.localPosition = _position;
+
+        _GameManager.BGMStop();
+        _audio.clip = _se1;
+        _audio.Play();
+    }
+}
