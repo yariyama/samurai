3cc9dacb3ffe5e0b904e682882502469bd0ce7e7
yariyama <86090612+yariyama@users.noreply.github.com>
2022-01-19 12:28:17 +0900

    Add files via upload

---
+++ GameMainManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMainManager : MonoBehaviour
+{
+    //音符プレファブ
+    public GameObject _Note_pf;
+    //音符オブジェクト
+    private GameObject _Note;
+    //音符スクリプト
+    private NoteManager _NoteManager;
+
+    //ステータス
+    private int _st;
+    //音符ステータス
+    public bool[] _note_st = new bool[30];
+
+    //繰り返し用
+    private int i;
+    //タイマー
+    public float _timer;
+
+    //_st=1-基本
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _st = 1;
+        _timer = 0;
+
+        for (i=1;i<30;i++)
+        {
+            _note_st[i] = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (_st==1)
+        {
+            _timer += Time.deltaTime;
+
+            if (_timer>=2)
+            {
+                _timer = 0;
+                NoteSet();
+            }
+        }
+    }
+
+    //音符セット
+    public void NoteSet()
+    {
+        _Note = Instantiate(_Note_pf) as GameObject;
+        for (i=0;i<30;i++) {
+            if (!_note_st[i])
+            {
+                _note_st[i] = true;
+                _Note.name = "Note" + i;
+                _NoteManager = _Note.GetComponent<NoteManager>();
+                _NoteManager._ver = i;
+                _NoteManager.ActiveSet(1);
+                break;
+            }
+        }
+    }
+}
---
+++ NoteManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    //座標
+    private Vector2 _position;
+    //アニメーション
+    private Animator _animator;
+
+    //バージョン
+    public int _ver;
+    //ステータス
+    public int _st;
+    //ラインNO
+    private int _line_no;
+    //色タイプ
+    private int _color_tp;
+
+    //ノーマルスピード
+    public float _n_speed=5;
+
+    //_st=1-基本移動
+
+    void Awake()
+    {
+        _position = transform.position;
+        _animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        //基本移動
+        if (_st==1)
+        {
+            transform.Translate(-_n_speed/50,0,0);
+        }
+    }
+
+    //アクティブ化
+    public void ActiveSet(int _no1)
+    {
+        _line_no = _no1;
+        _st = 1;
+        _color_tp = Random.Range(1,4);
+        if (_color_tp==1) {
+            _animator.Play("red_base");
+        }
+        else if (_color_tp == 2)
+        {
+            _animator.Play("blue_base");
+        }
+        else if (_color_tp == 3)
+        {
+            _animator.Play("yellow_base");
+        }
+
+        _position.x = 7.6f;
+        _position.y = 3.5f-2*(_line_no-1);
+        transform.position = _position;
+    }
+}
