e45d22877cba856a52f8f2ed83de3e7a30d252bc
yariyama <86090612+yariyama@users.noreply.github.com>
2022-02-25 12:12:59 +0900

    Add files via upload

---
+++ PlayerManager2.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager2 : MonoBehaviour
+{
+    //アニメーター
+    private Animator _animator;
+    //座標
+    private Vector3 _position;
+
+    //ステータス
+    private int _st;
+    //移動スピード
+    public float _speed;
+    //移動量X
+    private float _vx;
+    //移動量Z
+    private float _vz;
+
+    //_st=1-基本形
+    //_st=2-移動
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _position = transform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _st = 1;
+        _vx = 0;
+        _vz = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _vx = 0;
+        _vz = 0;
+
+        if (Input.GetKey("right"))
+        {
+            _vx = _speed;
+        }
+        else if (Input.GetKey("left"))
+        {
+            _vx = -_speed;
+        }
+        if (Input.GetKey("up"))
+        {
+            _vz = _speed;
+        }
+        else if (Input.GetKey("down"))
+        {
+            _vz = -_speed;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (_st==1)
+        {
+            if (_vx!=0||_vz!=0)
+            {
+                _st = 2;
+                _animator.Play("Character_Walk");
+            }
+        }
+        else if (_st==2)
+        {
+            if (_vx==0 && _vz==0)
+            {
+                BaseSet();
+            }
+        }
+
+        //移動
+        if (_vx != 0 || _vz != 0)
+        {
+            transform.Translate(_vx/50,0,_vz/50);
+        }
+    }
+
+    public void BaseSet()
+    {
+        _st = 1;
+        _animator.Play("Character_Idle");
+    }
+}
