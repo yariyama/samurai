6c788099a2a7bb2e65a046ccbe420f5bd92f639a
yariyama <86090612+yariyama@users.noreply.github.com>
2022-04-01 14:42:03 +0900

    Add files via upload

---
+++ SensorManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensorManager : MonoBehaviour
+{
+    //ゾンビオブジェクト
+    private GameObject _Zombie;
+    //ゾンビスクリプト
+    private ZombieManager _ZombieManager;
+
+    void Awake()
+    {
+        _Zombie = transform.parent.gameObject;
+        _ZombieManager = _Zombie.GetComponent<ZombieManager>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if ((_ZombieManager._st==1 || _ZombieManager._st==2)&&other.gameObject.tag=="Player")
+        {
+            _ZombieManager.AttackSet();
+        }
+    }
+}
