e2357e1786e7759bcf103b5a2ca04936a25796bd
yariyama <86090612+yariyama@users.noreply.github.com>
2022-04-01 14:40:38 +0900

    Add files via upload

---
+++ PlayerManager.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerManager : MonoBehaviourPunCallbacks
+{
+    //回転スクリプト
+    private RotateManager _RotateManager;
+    //FPSキャラクターオブジェクト
+    private GameObject _FPS_Character;
+    //FPSキャラクタースクリプト
+    private FPS_CharacterManager _FPS_CharacterManager;
+    //弾痕プレハブ
+    public GameObject _BulletHolePrefab;
+    //弾痕オブジェクト
+    private GameObject _BulletHole;
+    //サブカメラ
+    private GameObject _SubCamera;
+
+    //アニメーター
+    private Animator _animator;
+    //座標
+    private Vector3 _position;
+    //サブカメラ座標
+    private Vector3 _sub_camera_position;
+    //サブカメラ正面角度
+    private Vector3 _sub_camera_forword_angle;
+
+    //ステータス
+    private int _st;
+    //バージョン
+    public int _ver;
+    //移動スピード
+    public float _speed;
+    //移動量X
+    private float _vx;
+    //移動量Z
+    private float _vz;
+
+    //_st=1-基本形
+    //_st=2-移動
+    //_st=3-ショット
+
+    void Awake()
+    {
+        _RotateManager = GetComponent<RotateManager>();
+        _FPS_Character = transform.Find("FPS_Character").gameObject;
+        _FPS_CharacterManager = _FPS_Character.GetComponent<FPS_CharacterManager>();
+        _SubCamera = transform.Find("SubCamera").gameObject;
+
+        _animator = GetComponent<Animator>();
+        _position = transform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _st = 1;
+        _vx = 0;
+        _vz = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _vx = 0;
+        _vz = 0;
+
+        if (Input.GetKey("right"))
+        {
+            _vx = _speed;
+        }
+        else if (Input.GetKey("left"))
+        {
+            _vx = -_speed;
+        }
+        if (Input.GetKey("up"))
+        {
+            _vz = _speed;
+        }
+        else if (Input.GetKey("down"))
+        {
+            _vz = -_speed;
+        }
+
+        if (Input.GetKey("space"))
+        {
+            if (_st==1)
+            {
+                _st = 3;
+                //_animator.Play("Character_AimPose");
+                _animator.SetBool("shoot_flag", true);
+                _FPS_CharacterManager.ShootSet();
+
+                BulletHoleSet();
+            }
+            _RotateManager.ShootRot();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (_st==1)
+        {
+            if (_vx!=0||_vz!=0)
+            {
+                _st = 2;
+                //_animator.Play("Character_Walk");
+                _animator.SetBool("walk_flag",true);
+            }
+        }
+        else if (_st==2)
+        {
+            if (_vx==0 && _vz==0)
+            {
+                BaseSet();
+            }
+        }
+
+        //移動
+        if (_vx != 0 || _vz != 0)
+        {
+            transform.Translate(_vx/50,0,_vz/50);
+            _position = transform.position;
+            _position.y = 0;
+            transform.position = _position;
+        }
+    }
+
+    public void BaseSet()
+    {
+        _st = 1;
+        //_animator.Play("Character_Idle");
+        _animator.SetBool("walk_flag", false);
+        _animator.SetBool("shoot_flag", false);
+    }
+
+    public void ActiveSet(int _no)
+    {
+        _ver = _no;
+        if (_ver==2)
+        {
+            transform.position = new Vector3(0,0,5);
+            transform.localEulerAngles = new Vector3(0,180,0);
+            _RotateManager._angle = transform.localEulerAngles;
+        }
+    }
+
+    private void BulletHoleSet()
+    {
+        RaycastHit hit;
+
+        _sub_camera_position = _SubCamera.transform.position;
+        _sub_camera_forword_angle = _SubCamera.transform.forward;
+
+        Ray ray = new Ray(_sub_camera_position, _sub_camera_forword_angle);
+
+        //レイ確認用
+        Debug.DrawRay(ray.origin,ray.direction*10,Color.red,5);
+
+        if (Physics.Raycast(ray,out hit,Mathf.Infinity))
+        {
+            //_BulletHole = Instantiate(_BulletHolePrefab);
+            _BulletHole = PhotonNetwork.Instantiate("Prefabs/bulletHole", Vector3.zero, Quaternion.identity, 0);
+            //位置
+            _BulletHole.transform.position = hit.point;
+            //角度
+            _BulletHole.transform.rotation = Quaternion.FromToRotation(Vector3.forward, hit.normal);
+        }
+    }
+}
---
+++ SimplePun.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class SimplePun : MonoBehaviourPunCallbacks
+{
+    //プレイヤーオブジェクト
+    private GameObject _Player;
+    //プレイヤースクリプト
+    private PlayerManager _PlayerManager;
+    //回転スクリプト
+    private RotateManager _RotateManager;
+    //サブカメラオブジェクト
+    private GameObject _SubCamera;
+    //FPSキャラクターオブジェクト
+    private GameObject _FPS_Character;
+    //FPSキャラクタースクリプト
+    private FPS_CharacterManager _FPS_CharacterManager;
+    //マイNOオブジェクト
+    private GameObject _MyNo;
+    //ゾンビオブジェクト
+    private GameObject _Zombie;
+    //ゾンビスクリプト
+    private ZombieManager _ZombieManager;
+
+    //サブカメラカメラ
+    private Camera _sub_camera_camera;
+    //マイNOテキスト
+    private Text _my_no_text;
+
+    void Awake()
+    {
+        _MyNo = GameObject.Find("MyNo");
+        _my_no_text = _MyNo.GetComponent<Text>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        //旧バージョンでは引数必須でしたが、PUN2では不要です。
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    void OnGUI()
+    {
+        //ログインの状態を画面上に出力
+        GUILayout.Label(PhotonNetwork.NetworkClientState.ToString());
+    }
+
+
+    //ルームに入室前に呼び出される
+    public override void OnConnectedToMaster()
+    {
+        // "room"という名前のルームに参加する（ルームが無ければ作成してから参加する）
+        PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions(), TypedLobby.Default);
+    }
+
+    //ルームに入室後に呼び出される
+    public override void OnJoinedRoom()
+    {
+        //ナンバーリング
+        int _s = 0;
+        foreach (var player in PhotonNetwork.PlayerList)
+        {
+            ++_s;
+        }
+        _my_no_text.text = _s.ToString("00");
+
+        //キャラクターを生成
+        _Player = PhotonNetwork.Instantiate("Prefabs/Player", Vector3.zero, Quaternion.identity, 0);
+        _Player.name = "Player" + _s;
+
+        _SubCamera = _Player.transform.Find("SubCamera").gameObject;
+        _FPS_Character = _Player.transform.Find("FPS_Character").gameObject;
+        
+        _Zombie= PhotonNetwork.Instantiate("Prefabs/Zombie", Vector3.zero, Quaternion.identity, 0);
+        _Zombie.name = "Zombie" + _s;
+
+        //自分だけが操作できるようにスクリプトを有効にする
+        _PlayerManager = _Player.GetComponent<PlayerManager>();
+        _RotateManager = _Player.GetComponent<RotateManager>();
+        _PlayerManager.enabled = true;
+        _RotateManager.enabled = true;
+        _sub_camera_camera = _SubCamera.GetComponent<Camera>();
+        _sub_camera_camera.enabled = true;
+        _FPS_CharacterManager = _FPS_Character.GetComponent<FPS_CharacterManager>();
+        _FPS_CharacterManager.enabled = true;
+        
+        _ZombieManager = _Zombie.GetComponent<ZombieManager>();
+        _ZombieManager.enabled = true;
+        _ZombieManager.ActiveSet(_s);
+        
+        
+        //初期化
+        _PlayerManager.ActiveSet(_s);
+    }
+}
+
