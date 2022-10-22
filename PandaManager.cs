8a22af3093af4276e8e5db72619d31703cd5514e
yariyama <86090612+yariyama@users.noreply.github.com>
2022-10-22 12:33:27 +0900

    Add files via upload

---
+++ PandaManager.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PandaManager : MonoBehaviour
+{
+    //アニメーター
+    private Animator _animator;
+
+    //ステータス
+    private int _st;
+    //移動量z
+    private float _vz;
+    //移動スピード
+    public float _w_speed;
+    //回転量
+    private float _angle;
+    //回転スピード
+    public float _a_speed;
+
+    //_st=1-基本形
+    //_st=2-移動
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _st = 1;
+        _animator.Play("Base");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _vz = 0;
+        _angle = 0;
+
+        if (Input.GetKey("up"))
+        {
+            _vz = _w_speed;
+        }
+        else if (Input.GetKey("down"))
+        {
+            _vz = -_w_speed;
+        }
+
+        if (Input.GetKey("right"))
+        {
+            _angle = _a_speed;
+        }
+        else if (Input.GetKey("left"))
+        {
+            _angle = -_a_speed;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (_st==1)
+        {
+            if (_vz!=0||_angle!=0)
+            {
+                _st = 2;
+                _animator.Play("Walk");
+            }
+        }
+        else if (_st==2)
+        {
+            if (_vz == 0 && _angle == 0)
+            {
+                _st = 1;
+                _animator.Play("Base");
+            }
+        }
+
+
+        if (_vz!=0) {
+            transform.Translate(0, 0, _vz / 50);
+        }
+        if (_angle != 0)
+        {
+            transform.Rotate(0, _angle / 50, 0);
+        }
+    }
+}
