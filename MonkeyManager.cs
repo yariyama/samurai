8039a82d4ddd3455e7f4866363dc96844e6b4263
yariyama <86090612+yariyama@users.noreply.github.com>
2022-11-12 20:28:30 +0900

    Add files via upload

---
+++ MonkeyManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkeyManager : MonoBehaviour
+{
+    //アニメーター
+    private Animator _animator;
+    //角度
+    private Vector3 _angle;
+
+    //ステータス
+    public int _st;
+    //移動スピード
+    public float _w_speed;
+    //方向
+    private int _dire;
+    //タイマー
+    private float _timer;
+    //ランダム値
+    private int _ran;
+
+    //_st=1-基本形
+    //_st=2-移動
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _angle = transform.localEulerAngles;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _st = 1;
+        _dire = 1;
+        _timer = 0;
+        _animator.Play("Base");
+        _angle.y = 270;
+        transform.localEulerAngles = _angle;
+    }
+
+    void FixedUpdate()
+    {
+        if (_st==1)
+        {
+            _timer += Time.deltaTime;
+            if (_timer>=1)
+            {
+                _ran = Random.Range(0,5);
+                if (_ran==1)
+                {
+                    _timer = 0;
+                    _st = 2;
+                    _animator.Play("Walk");
+                }
+            }
+        }
+        else if (_st==2)
+        {
+            transform.Translate(0,0,_w_speed/50);
+        }
+    }
+}
