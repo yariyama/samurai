f3e4889f13ec05acb4d2a90c342a2af482139f9a
yariyama <86090612+yariyama@users.noreply.github.com>
2022-11-18 23:02:10 +0900

    Add files via upload

---
+++ MonkeySensorManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkeySensorManager : MonoBehaviour
+{
+    //モンキーオブジェクト
+    private GameObject _Monkey;
+    //モンキースクリプト
+    private MonkeyManager _MonkeyManager;
+
+
+    void Awake()
+    {
+        _Monkey = transform.parent.gameObject;
+        _MonkeyManager = _Monkey.GetComponent<MonkeyManager>();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag=="Cube" && _MonkeyManager._st==2)
+        {
+            _MonkeyManager.TurnSet();
+        }
+    }
+}
