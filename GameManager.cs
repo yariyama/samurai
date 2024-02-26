291c4e3bb6c6bdebd6dd0e1d37d32beaec0ce3fc
yariyama <86090612+yariyama@users.noreply.github.com>
2024-02-26 15:41:34 +0900

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //スコア
+    public static int _score=0;
+    //残機
+    public static int _zan_c=2;
+    //ステージ
+    public static int _stage = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+}
